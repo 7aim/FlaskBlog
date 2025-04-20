a2ea8055683f20d4ea3eeb33c9fb6969a46e878f
visonn336 <visonn336@gmail.com>
2025-04-20 17:30:25 +0400

    gitignore added

--- Blog/flaskblog/app.py
+++ Blog/flaskblog/app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, request, redirect, abort
-from utils import read_posts, create_post, delete_post
+from utils import read_posts, create_post, delete_post # ? write_posts()
 
 app = Flask(__name__)
 
-# Ana Sayfa
+# Main page 
 @app.route('/')
 def home():
     posts = read_posts()
     return render_template('index.html', posts=posts)
 
-# Gönderi Oluşturma
-@app.route('/create', methods=['GET', 'POST'])
+# Create article
+@app.route('/create', methods=['GET', 'POST']) # ? methods
 def create():
     if request.method == 'POST':
         create_post(
@@ -20,20 +20,20 @@ def create():
         return redirect('/')
     return render_template('create.html')
 
-# Tekil Gönderi
+# Article
 @app.route('/post/<int:post_id>')
 def post(post_id):
     posts = read_posts()
-    post = next((p for p in posts if p["id"] == post_id), None)
+    post = next((p for p in posts if p["id"] == post_id), None) # ?
     if not post:
         abort(404)
     return render_template('post.html', post=post)
 
-# Gönderi Silme (Örnek)
+# Delete article
 @app.route('/delete/<int:post_id>', methods=['GET'])
 def delete(post_id):
     delete_post(post_id)
-    return redirect('/')
+    return redirect('/') # ?
 
 if __name__ == '__main__':
     app.run(debug=True)
